a36106349b893e88933014f03a32750ef46603b8
gus-bms <jgk03077@naver.com>
2022-12-25 02:00:22 +0900

    user 조회, 저장api 및 bodyparser 추가

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 const app = express();
 const PORT = process.env.port || 8000;
 const mysql = require("mysql");
@@ -10,6 +12,9 @@ let corsOptions = {
 };
 
 app.use(cors(corsOptions));
+// body-parser 모듈은 body 안에 있는 json을 파싱해준다.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -48,19 +53,77 @@ app.get("/api/selectBoardList", async function (req, res) {
 });
 
 app.get("/api/selectSpotList", async function (req, res) {
-  console.log(req.query);
-  const sql = `
+  try {
+    const sql = `
     SELECT *
     FROM SPOT 
     WHERE 1=1
     AND address_dong LIKE '%${req.query.address_dong}%'
-`;
-  connection.query(sql, function (err, rows) {
-    if (err) throw err;
-    console.log(rows);
-    res.json({
-      r: true,
-      list: rows,
+  `;
+    connection.query(sql, function (err, rows) {
+      if (err) throw err;
+      console.log(rows);
+      res.json({
+        r: true,
+        list: rows,
+      });
     });
-  });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/api/selectUser", async (req, res) => {
+  console.log(req.body);
+  try {
+    const sql = `
+    SELECT *
+    FROM user 
+    WHERE 1=1
+    AND uid = ${req.body.id}
+  `;
+    connection.query(sql, (err, rows) => {
+      console.log(rows);
+      Array.isArray(rows) && rows.length != 0
+        ? res.json({
+            r: true,
+          })
+        : res.json({
+            r: false,
+          });
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      r: false,
+    });
+  }
+});
+
+app.post("/api/insertUser", async (req, res) => {
+  console.log("insertUser");
+
+  try {
+    const sql = `
+      INSERT INTO user
+      (uid, name, email, profile_url)
+      VALUES
+      (${req.body.uid}, '${req.body.name}', '${req.body.email}', '${req.body.profile_url}')
+    `;
+    connection.query(sql, (err, rows) => {
+      err
+        ? (console.log(err),
+          res.json({
+            r: false,
+          }))
+        : res.json({
+            r: true,
+          });
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      r: false,
+    });
+  }
 });
