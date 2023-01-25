b84121a72d78e078c207fe1534cf4b0dd3df660a
gus-bms <jgk03077@naver.com>
2023-01-25 16:58:33 +0900

    spot, user 수정

--- src/services/spot.js
+++ src/services/spot.js
@@ -7,10 +7,10 @@ module.exports = async function (app) {
   const db = await require("../db/db").connection;
 
   // DB에 저장된 장소 목록 조회
-  app.get("/api/selectSpotList", async (req, res) => {
+  app.get("/api/spot/selectSpotList", async (req, res) => {
     try {
       db.getConnection(async (err, connection) => {
-        console.log(req.query.address_dong);
+        console.log("동 ==", req.query.address_dong);
         const sql = `
           SELECT *
           FROM SPOT
@@ -19,6 +19,7 @@ module.exports = async function (app) {
         `;
 
         connection.query(sql, function (err, rows) {
+          console.log(sql);
           if (err) throw err;
           console.log(rows);
           connection.release();
@@ -31,6 +32,42 @@ module.exports = async function (app) {
     } catch (err) {
       console.log(err);
       connection.release();
+    }
+  });
+
+  // DB에 log를 저장합니다.
+  app.post("/api/spot/insertSpot", async (req, res) => {
+    try {
+      console.log("insertSpot", req.body);
+      db.getConnection(async (err, connection) => {
+        const sql = `
+          INSERT INTO spot
+          (name, category, address, address_dong, reg_id, reg_dt)
+          VALUES ('${req.body.name}', '${req.body.type}', '${req.body.address}', '${req.body.address_dong}', ${req.body.user_pk}, now())
+        `;
+        connection.query(sql, async function (err, rows) {
+          connection.release();
+          if (err) {
+            console.log(err);
+            res.json({
+              r: false,
+            });
+            return;
+          }
+
+          console.log(rows);
+          res.json({
+            r: true,
+            // id: rows.insertId,
+          });
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      connection.release();
+      res.json({
+        r: false,
+      });
     }
   });
 };
--- src/services/user.js
+++ src/services/user.js
@@ -2,14 +2,17 @@ module.exports = async function (app) {
   const db = await require("../db/db").connection;
 
   // DB에 저장된 USER 조회
-  app.post("/api/selectUser", async (req, res) => {
+  app.get("/api/selectUser", async (req, res) => {
     try {
+      let param = Object.keys(req.query);
+      let value = req.query[param];
+      console.log(value);
       db.getConnection(async (err, connection) => {
         const sql = `
           SELECT *
           FROM user
           WHERE 1=1
-          AND uid = '${req.body.id}'
+          AND ${param} = '${value}'
         `;
         console.log(sql);
         connection.query(sql, (err, rows) => {
@@ -19,6 +22,9 @@ module.exports = async function (app) {
             ? res.json({
                 r: true,
                 uid: rows[0].uid,
+                profileUrl: rows[0].profile_url,
+                userPk: rows[0].user_pk,
+                intro: rows[0].intro,
               })
             : res.json({
                 r: false,
@@ -47,6 +53,111 @@ module.exports = async function (app) {
           VALUES
           ('${req.body.uid}', '${req.body.name}', '${email}', '${profileUrl}', now())
         `;
+        connection.query(sql, (err, rows) => {
+          connection.release();
+          err
+            ? (console.log(err),
+              res.json({
+                r: false,
+              }))
+            : res.json({
+                r: true,
+              });
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      connection.release();
+      res.json({
+        r: false,
+      });
+    }
+  });
+
+  // refresh token 정보 저장
+  app.post("/api/user/updateRefreshToken", async (req, res) => {
+    console.log("refresh token update");
+    const refreshToken = req.body.refreshToken;
+    const uid = req.body.uid;
+    try {
+      db.getConnection(async (err, connection) => {
+        const sql = `
+            UPDATE user
+            set refresh_token = '${refreshToken}'
+            WHERE
+            uid = '${uid}'
+          `;
+        connection.query(sql, (err, rows) => {
+          connection.release();
+          err
+            ? (console.log(err),
+              res.json({
+                r: false,
+              }))
+            : res.json({
+                r: true,
+              });
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      connection.release();
+      res.json({
+        r: false,
+      });
+    }
+  });
+
+  // 유저 정보 수정
+  app.post("/api/user/update", async (req, res) => {
+    console.log("user update");
+
+    const intro = req.body.intro;
+    const uid = req.body.uid;
+    console.log(intro);
+    try {
+      db.getConnection(async (err, connection) => {
+        const sql = `
+            UPDATE user
+            set intro = '${intro}'
+            WHERE
+            uid = '${uid}'
+          `;
+        connection.query(sql, (err, rows) => {
+          console.log(rows);
+          connection.release();
+          err
+            ? (console.log(err),
+              res.json({
+                r: false,
+              }))
+            : res.json({
+                r: true,
+              });
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      connection.release();
+      res.json({
+        r: false,
+      });
+    }
+  });
+
+  // profile 변경 정보 저장
+  app.post("/api/user/updateProfile", async (req, res) => {
+    console.log("profile update");
+    const url = req.body.url;
+    const uid = req.body.uid;
+    try {
+      db.getConnection(async (err, connection) => {
+        const sql = `
+            UPDATE user
+            set profile_url = '${url}'
+            WHERE
+            uid = '${uid}'
+          `;
         connection.query(sql, (err, rows) => {
           connection.release();
           err
